299d06fa7d8b6de5f6862c7155a7df44a720fcd2
Bradley Morin <bradleymorin@gmail.com>
2025-01-11 01:59:48 -0700

    Add Todo and Project objects.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./style.css";
+
+class Todo {
+  constructor() {
+    this.title = "Default";
+    this.description = "Default";
+    this.dueDate = "Next week";
+    this.priority = "High";
+    this.notes = "";
+    this.complete = false;
+  }
+}
+
+class Project {
+  constructor() {
+    this.name = "Project 1";
+    this.todoList = [];
+    this.todoList.push(new Todo());
+  }
+}
+
+const project = new Project();
+console.log(project.todoList[0].title);
