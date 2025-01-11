ce65b664e0146de18be89b0eafa686a675394910
Bradley Morin <bradleymorin@gmail.com>
2025-01-11 16:25:49 -0700

    Add Todo setters

--- src/index.js
+++ src/index.js
@@ -3,27 +3,36 @@ import { format, subDays } from 'date-fns';
 
 class Todo {
   constructor(
-    title = "Your Todo",
-    dueDate = subDays(new Date(), -1),
+    title = "My Todo",
+    dueDate = new Date(),
     description = "Todo Description",
     priority,
   ) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    if (priority !== null) {this.priority = priority}
-    this.notes = "";
-    this.complete = false;
+    this.setTitle(title);
+    this.setDescription(description);
+    this.setDueDate(dueDate);
+    this.setPriority(priority);
+    this.setIncomplete();
   }
+
+  setTitle = (title) => {this.title = title;}
+  setDueDate = (dueDate) => {this.dueDate = dueDate;}
+  setDescription = (description) => {this.description = description;}
+
+  setComplete = () => {this.complete = true;}
+  setIncomplete = () => {this.complete = false;}
+
+  setPriority = (priority) => {this.priority = priority;}
 }
 
 class Project {
-  constructor(name = "Project 1") {
+  constructor(name = "My Project") {
     this.name = name;
     this.todoList = [new Todo()];
   }
 }
 
 const project = new Project();
-const formattedDate = format(project.todoList[0].dueDate, 'yyyy-MM-dd');
+const todo = project.todoList[0];
+const formattedDate = format(todo.dueDate, 'yyyy-MM-dd');
 console.log(formattedDate);
