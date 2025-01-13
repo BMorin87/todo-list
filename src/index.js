acc3d97e6b46593f5379a9f0a3715eee7fd03723
Bradley Morin <bradleymorin@gmail.com>
2025-01-12 17:31:01 -0700

    Add ScreenController
    "Add Project" button adds a new project to the dropdown.
    "Add Todo" button adds a new Todo to the current project.

--- src/index.js
+++ src/index.js
@@ -15,14 +15,14 @@ class Todo {
     this.setIncomplete();
   }
 
-  setTitle = (title) => {this.title = title;}
-  setDueDate = (dueDate) => {this.dueDate = dueDate;}
-  setDescription = (description) => {this.description = description;}
+  setTitle = (title) => { this.title = title; }
+  setDueDate = (dueDate) => { this.dueDate = dueDate; }
+  setDescription = (description) => { this.description = description; }
 
-  setComplete = () => {this.complete = true;}
-  setIncomplete = () => {this.complete = false;}
+  setComplete = () => { this.complete = true; }
+  setIncomplete = () => { this.complete = false; }
 
-  setPriority = (priority) => {this.priority = priority;}
+  setPriority = (priority) => { this.priority = priority; }
 }
 
 class Project {
@@ -32,18 +32,68 @@ class Project {
   }
 }
 
-const project = new Project();
-const todo = project.todoList[0];
-const formattedDate = format(todo.dueDate, "yyyy-MM-dd")
+class ScreenController {
+  constructor() {
+    this.projectList = [];
+    this.LoadDefaultProject()
+    this.activeProject = this.projectList[0];
 
-const todoContainer = document.getElementById("todoContainer");
-const todoCard = document.createElement("div");
-todoCard.classList.add("todoCard")
-todoCard.innerHTML = `
-<p class="todoTitle">${todo.title}</p>
-<p class="todoDescription">${todo.description}</p>
-<p class="todoDueDate">${formattedDate}</p>
-<p class="todoPriority">${todo.priority}</p>
-<label><input type="checkbox" class="completion"> Todo complete</label>
-`
-todoContainer.appendChild(todoCard);
+    const addProjectButton = document.getElementById("addProject");
+    const addTodoButton = document.getElementById("addTodo");
+    addProjectButton.addEventListener('click', () => this.AddProjectClick());
+    addTodoButton.addEventListener('click', () => this.AddTodoClick());
+  }
+
+  LoadDefaultProject() {
+    const defaultProject = new Project();
+    this.projectList.push(defaultProject);
+
+    // Create a default todo card.
+    const todo = this.projectList[0].todoList[0];
+    const card = this.CreateTodoCard(todo);
+    this.AddCardToContainer(card);
+
+    // Add the default project to the dropdown menu.
+    this.AddProjectToSelector(defaultProject)
+  }
+
+  AddProjectClick() {
+    const newProject = new Project("New Project");
+    this.projectList.push(newProject);
+    this.AddProjectToSelector(newProject);
+  }
+
+  AddTodoClick() {
+    const newTodo = new Todo("New Todo");
+    const newCard = this.CreateTodoCard(newTodo);
+    this.AddCardToContainer(newCard);
+  }
+
+  CreateTodoCard(todo) {
+    const todoCard = document.createElement("div");
+    todoCard.classList.add("todoCard")
+    const formattedDate = format(todo.dueDate, "yyyy-MM-dd")
+    todoCard.innerHTML = `
+    <h3 class="todoTitle">${todo.title}</h3>
+    <p class="todoDescription">${todo.description}</p>
+    <p class="todoDueDate">${formattedDate}</p>
+    <p class="todoPriority">${todo.priority}</p>
+    <label><input type="checkbox" class="completion"> Todo complete</label>
+    `
+    return todoCard;
+  }
+
+  AddCardToContainer(card) {
+    const todoContainer = document.getElementById("todoContainer");
+    todoContainer.appendChild(card);
+  }
+
+  AddProjectToSelector(project) {
+    const selector = document.getElementById("projectSelector");
+    const newOption = document.createElement('option');
+    newOption.text = project.name;
+    selector.add(newOption);
+  }
+}
+
+const controller = new ScreenController();
